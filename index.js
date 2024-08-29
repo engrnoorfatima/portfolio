a1f99d73eeb30ba52e1b4e3a6d75f54af282e695
engrnoorfatima <147391066+engrnoorfatima@users.noreply.github.com>
2024-08-29 12:24:55 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ burger = document.querySelector('.burger')
 navList = document.querySelector('.nav-list')
 btnResp = document.querySelector('.btn-resp')
 sticky = document.querySelector('.sticky')
+logo = document.querySelector('.logo')
 
 burger.addEventListener('click', ()=>{
    btnResp.classList.toggle('v-class-resp');
    navList.classList.toggle('v-class-resp');
+   logo.classList.toggle('v-class-resp');
    sticky.classList.toggle('h-nav-resp');
 })
